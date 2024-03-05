ac83edb41b2fee261eb2a7ff01f92e3ee47dd62d
Lincoln Barsotti <107706237+LincolnBarsotti@users.noreply.github.com>
2024-03-05 20:19:48 -0300

    Update

--- src/main/java/org/example/storeapi/infra/security/SecurityFilterLogin.java
+++ src/main/java/org/example/storeapi/infra/security/SecurityFilterLogin.java
@@ -4,6 +4,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.storeapi.domain.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,8 +16,37 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilterLogin extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository repository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJwt = recuperarToken(request);
+        if (tokenJwt != null){
+            var email = tokenService.getEmail(tokenJwt);
+            var usuario = repository.findByEmail(email);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario,
+                    null,
+                    usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
 
     }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null){
+            return authorizationHeader.replace("Bearer", "").trim();
+        }
+        return null;
+    }
+
 }
--- src/main/java/org/example/storeapi/infra/security/TokenService.java
+++ src/main/java/org/example/storeapi/infra/security/TokenService.java
@@ -1,9 +1,51 @@
 package org.example.storeapi.infra.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.example.storeapi.domain.user.Usuario;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
 
+    private String secret = "1234";
+
+    public String gerarToken(Usuario usuario){
+
+        try{
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("Store API")
+                    .withSubject(usuario.getEmail())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar o token", exception);
+        }
+    }
+
+    public String getEmail(String tokenJWT){
+        try {
+            var algoritimo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritimo)
+                    .withIssuer("Store API")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("JWT inválido ou expirado");
+        }
+
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
 
 }
